410c5b1f067de9ccc31c83cf3026d9affb0616fe
frederichdk <fw21deko@siena.edu>
2024-02-15 12:01:14 -0500

    katchow

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Card from './Card';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Card from "./components/Card";
+import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import App from './App';
+import App from "./containers/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-        <App/>
+    <App />
   </React.StrictMode>
 );
 
